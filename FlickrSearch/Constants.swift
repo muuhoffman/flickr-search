514cf834454eb5b130a056d663e261f28fa612ad
Matthew Hoffman <muuhoffman@gmail.com>
2017-06-20 09:31:40 -0700

    Programmatic VC init, orientation and device type detection

---
+++ FlickrSearch/Constants.swift
@@ -0,0 +1,45 @@
+//
+//  Constants.swift
+//  FlickrSearch
+//
+//  Created by Matthew Hoffman on 6/20/17.
+//  Copyright © 2017 Hoffware. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    struct Color {
+        static let SearchViewControllerBackground = UIColor.yellow
+    }
+    
+    struct Device {
+        static var idiom: UIUserInterfaceIdiom {
+            get {
+                return UIDevice.current.userInterfaceIdiom
+            }
+        }
+        static var orientation: UIInterfaceOrientation {
+            get {
+                return UIApplication.shared.statusBarOrientation
+            }
+        }
+
+        static var screenSize: CGRect {
+            get {
+                return UIScreen.main.bounds
+            }
+        }
+        static var screenWidth: CGFloat {
+            get {
+                return screenSize.width
+            }
+        }
+        static var screenHeight: CGFloat {
+            get {
+                return screenSize.height
+            }
+        }
+    }
+}
--- FlickrSearch/ViewController.swift
+++ FlickrSearch/ViewController.swift
@@ -13,6 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.title = "Flickr Search"
+        view.backgroundColor = Constants.Color.SearchViewControllerBackground
+        
+        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
+        button.backgroundColor = .green
+        button.setTitle("Test Button", for: .normal)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        
+        self.view.addSubview(button)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(orientationChanged(notification:)), name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func buttonAction(sender: UIButton!) {
+        printDeviceInfo()
+    }
+    
+    func orientationChanged(notification: NSNotification) {
+        printDeviceInfo()
+    }
+    
+    func printDeviceInfo() {
+        switch Constants.Device.idiom {
+        case .phone:
+            print("iphone")
+        case .pad:
+            print("ipad")
+        default:
+            print("other idiom")
+        }
+        
+        switch Constants.Device.orientation {
+        case .landscapeLeft, .landscapeRight:
+            print("landscape")
+        case .portrait, .portraitUpsideDown:
+            print("portrait")
+        default:
+            print("unknown")
+        }
+        
+        print("Height", Constants.Device.screenHeight)
+        print("Width", Constants.Device.screenWidth)
+    }
 
 }
 
